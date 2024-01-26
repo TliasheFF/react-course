fe7b18e7c432b1740239ff2b2860c64bfb8fe0fe
Tliashev.FF <f.tlyashev@gmail.com>
2024-01-26 19:26:58 +0500

    fix button component

--- src/components/button/component.jsx
+++ src/components/button/component.jsx
@@ -4,15 +4,7 @@ import classNames from "classnames";
 
 export const Button = ({ onClick, children, className, disabled, size = Size.m }) => {
   return (
-    <button
-      disabled={disabled}
-      onClick={onClick}
-      className={classNames(styles.root, className, {
-        [styles.s]: size === Size.s,
-        [styles.m]: size === Size.m,
-        [styles.l]: size === Size.l,
-      })}
-    >
+    <button disabled={disabled} onClick={onClick} className={classNames(styles.root, className, styles[size])}>
       {children}
     </button>
   );
