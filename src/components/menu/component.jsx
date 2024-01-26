4cab296db04b3839c1710075048f46b5c00ecfc6
Tliashev.FF <f.tlyashev@gmail.com>
2024-01-26 19:05:53 +0500

    fix styles for menu

--- src/components/menu/component.jsx
+++ src/components/menu/component.jsx
@@ -1,9 +1,10 @@
 import { Dish } from "../dish/component";
 import styles from "./styles.module.scss";
+import classNames from "classnames";
 
-export const Menu = ({ menu }) => {
+export const Menu = ({ menu, className }) => {
   return (
-    <ul className={styles.root}>
+    <ul className={classNames(className)}>
       {menu.map((dish) => (
         <li key={dish.id}>
           <Dish className={styles.dishes} dish={dish} />
--- src/components/restaurant/component.jsx
+++ src/components/restaurant/component.jsx
@@ -8,7 +8,7 @@ export const Restaurant = ({ restaurant, activeTab, className }) => {
     <div className={classNames(styles.root, activeTab === restaurant.id ? className.tabActive : className.tabDefault)}>
       <h2 className={styles.restaurant_name}>{restaurant.name}</h2>
       <h3>Меню:</h3>
-      <Menu menu={restaurant.menu} />
+      <Menu className={styles.restaurant_menu} menu={restaurant.menu} />
       <h3>Отзывы:</h3>
       <Reviews reviews={restaurant.reviews} />
     </div>
